c944efac0e033a860eee72f8c37eed7621ca40f1
Nedit <nedxha19@htl-shkoder.com>
2025-03-04 11:55:43 +0100

    Neue Änderugen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="api/burgen">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Visit <a href="api/burgen">Try our Api</a> to read the documentation</p>
